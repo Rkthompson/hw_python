ba054a5ba522c8354c9d79794e6c6797e2c1f163
Rkthompson <robertkellythompson@gmail.com>
2020-12-21 15:44:52 -0500

    Added Hello World statements using variables, while loop and if...else

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#This is a Hello World to test uploading Python file to GitHub
+firstName = "John"
+#firstName = "Jon"
+lastName = "Smith"
+fullName = (firstName + " " + lastName)
+
+index = 0
+while index == 0:
+    if fullName == "John Smith":
+        #only prints if full name is John Smith
+        print("Hello World, this is " + fullName)
+    else:
+        #else prints Hello World
+        print("Hello World")
+    index = index + 1
